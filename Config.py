baf7e3bf7ab8220a55abbe85a7c7eac7e4dfee9e
Niyaz11 <145466262+Niyaz11@users.noreply.github.com>
2023-09-19 21:39:41 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,11 +3,11 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "6213538"))
     API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", 6673102864:AAEsazdr8KNFm9caJrnxm-FjQ1Gu8lRtGu8)
+    STRING_SESSION = os.environ.get("STRING_SESSION", 1BVtsOMUBu0dHXUOuaSg53ZDNbjWGkkaO_uu3zeEwEeku2izYrHDrs0RZEL5m9jUj78dMNu3jSL8lxLNIfCxsUlfPEYrUhxaZVLwOCCAs931RxiN5WbcDYbQDNvmnR8L0ZyKV3DpI8qHeZLWKh55w2h3YbO4d4h1sQPOnvJBK5nwirlpBQyqZXy4S6auqKKwDOh_agFsMncDlLZHm4GyUryziMzGC63t7CsOlyPo3SyqB-EeFRBzMyKo0BGesnstgYYKs2ZRPbzaO1t-kxGcGTL-N25zCUt20PJJDn_8nz-4MWb5COvV6JWLZg7tq6bQ3cfvIDDq3m3KdnxrG0KNPoAjy31ZDEY4=)
+    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", )
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME",NOUZON_MUSSIC_BOT "")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
